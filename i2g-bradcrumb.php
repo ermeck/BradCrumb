1a8b8291d59b0b33b0762dc99c598eb84420833c
ermeck <ermeck@bk.ru>
2019-04-20 00:10:02 +0400

    update

--- i2g-bradcrumb.php
+++ i2g-bradcrumb.php
@@ -8,9 +8,15 @@
  */
 
 
+// КОММЕНТАРИИ ДВОЙНЫМ СЛЕШЕМ - МОИ ПОЯСНЕНИЯ САМОМУ СЕБЕ
+// КОММЕНТАРИИ ЗВЕЗДОЧКАМИ - ПОЯСНЕНИЯ АВТОРА
+
+
 
 // это второй хук с функцией 'wp_title', первый в файле functions.php поэтому,
 // ..чтобы он выполнился позже - задаем ему приоритет 20
+
+
 add_filter( 'wp_title', 'i2g_title', 20 );  
 
 function i2g_title ($title) {
@@ -29,8 +35,47 @@ function i2g_title ($title) {
     if ( is_home() || is_front_page() ) {
         // В тайтле - массив, чтобы проще было оперировать
         $title = array($site);
+        $title[] = 'test';
     }
 
+    /**
+     * ПОСТОЯННАЯ СТРАНИЦА
+     */
+    elseif ( is_page() ) {
+        $title = array( get_the_title(), $site, );
+    }
 
+    /**
+     * МЕТКА
+     */
+    // архив включает себя метки и еще много чего, засим метки нужно ставить ДО архива
+    elseif ( is_tag() ) {
+        // второй параметр false говорит, что мы не выводим, а возвращаем
+        $title = array( single_tag_title( 'Метка: ', false ), $site );
+    }
+
+    /**
+     * ПОИСК
+     */
+    elseif ( is_search() ) {
+        $title = array( 'Результаты поиска по запросу: ', get_search_query() );
+    }
+
+    /**
+     * 404
+     */
+    elseif ( is_404() ) {
+        $title = array('Страница не найдена');
+    }
+
+    /**
+     * АРХИВ
+     */
+    elseif ( is_archive() ) {
+        $title = array( 'Архив за: ' . get_the_time( "F Y" ), $site );
+    }
+
+    // крамсаем заголовок на кусочки и превращаем в строку с разделителем
+    $title = implode( $sep, $title );
     return $title;
 }
