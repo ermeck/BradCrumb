f899a9fa1c0400a911fb9ed6f6234dfcb5794dc2
ermeck <ermeck@bk.ru>
2019-04-19 18:21:13 +0400

    update

---
+++ i2g-bradcrumb.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Хлебные крошки в title
+ * Description: Добавляем полный путь в title страниц. Для корректной работы плагина, тег title шаблона header.php должен выглядеть так: &lt;title&gt;&lt;?php wp_title(); ?&gt;&lt;/title&gt;
+ * Plugin URI: http://ermeck.com
+ * Author: i2gun
+ * Author URI: http://ermeck.com
+ */
